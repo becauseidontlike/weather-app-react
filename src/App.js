54947f4f51f8f39d2ee10ba53ff1540e1aa876a2
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2023-01-22 19:34:47 +0100

    to check on netlify

--- src/App.js
+++ src/App.js
@@ -1,68 +1,86 @@
+import React, { useState } from 'react';
+import axios from "axios";
 import './App.css';
 import Footer from "./Footer";
 
 function App() {
+  const [city, setCity] = useState("");
+  const [temp, setTemp] = useState("");
+  const [description, setDescription] = useState("");
+  const [humidity, setHumidity] = useState("");
+  const [wind, setWind] = useState("");
+  const [icon, setIcon] = useState("");
+  const [loaded, setLoaded] = useState("");
+
+  let form = (
+    <form onSubmit={handleQuery}>
+      <input type="search" placeholder="City..." onChange={updateCity}></input>
+      <input type="submit" value="Search"></input>
+    </form>
+  );
+
+  function handleQuery(event) {
+    event.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6c8161756616103589832909859e4f86&units=metric`;
+    axios.get(url).then(showWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  function showWeather(response) {
+    setLoaded(true);
+    setTemp(response.data.main.temp);
+    setDescription(response.data.weather[0].description);
+    setHumidity(response.data.main.humidity);
+    setWind(response.data.wind.speed);
+    setIcon(`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+   );
+  }
   
+
+
+ if (loaded) {
   return (
 <div className="App">
-  <div className="container">
-        <div className="row align-items-start">
-          <div className="col-3">
-            <h1 className="myCity">Kraków</h1>
-          </div>
-          <div className="col-7">
-            <form action="" id="form-search">
-              <input
-                className="form-control"
-                type="text"
-                placeholder="City"
-                aria-label="default input example"
-                id="write-in"
-              />{" "}
-              <button
-                type="submit"
-                className="btn btn-secondary"
-                id="searching-city"
-              >
-                Search
-              </button>
-            </form>
-          </div>
-        </div>
-  
-    <br />
+    <div className="container">
+      <div>{form}</div>
+       
+      <br />
     <div className="row align-items-start">
       <div className="col-8 bold">
         <div>
           <div className="today">
             THURSDAY</div>LAST UPDATED: <span className="time"> 14:<span className="min">00</span></span><br /><span
             className="current">CURRENT
-            WEATHER</span>
+            WEATHER: {description}</span>
           <br />
         </div>
       </div>
       <div className="col-4">
-        <div className="addition">HUMIDITY: <span className="humid">60</span>% <br />WIND: <span className="speed">1</span>
+        <div className="addition">HUMIDITY: <span className="humid">{humidity}</span>% <br />WIND: <span className="speed">{wind}</span>
           m/s</div><br />
       </div>
     </div>
 
     <div className="row align-items-start">
       <div className="col-2">
+        <img src={icon} alt={description} />
         <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="weather" id="icon"></img>
       </div>
       <div className="col-4">
-        <h2><span className="units"><span className="actualTemp" id="celsius"></span><a href="#" id="celsiusT"
+        <h2><span className="units"><span className="actualTemp" id="celsius">{Math.round(temp)}</span><a href="https://www.shecodes.io" id="celsiusT"
               className="active active2">℃</a> |
-            <a href="#" id="fahrenheitT" className="active2">℉</a></span>
+            <a href="https://www.shecodes.io" id="fahrenheitT" className="active2">℉</a></span>
         </h2>
       </div>
     </div>
   </div>
 <Footer />
 </div>
-
   );
+}
 }
 
 export default App;
--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 export default function Footer (){
     return (
 <div className="Footer">
-    <footer className="sign">Coded with ☕&🍪 by WP<br /><a href="https://github.com/becauseidontlike/Weather-App"
+    <footer className="sign">Coded with ☕&🍪 by WP<br /><a href="https://github.com/becauseidontlike/weather-app-react"
     target="_blank" rel="noreferrer">Open-source
     code</a></footer>
     </div>
