f77ab5736db3de7c562c4604ece2bc1df1e207c0
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2023-01-22 19:02:49 +0100

    app copy from vanilla js to react

--- src/App.js
+++ src/App.js
@@ -1,19 +1,67 @@
 import './App.css';
-import Weather from './Weather';
+import Footer from "./Footer";
 
 function App() {
-  let city = "Krakow";
+  
   return (
-    <div className="App">
-      <header className="App-header">
-      <h1>It's me, hi!!!</h1>
-      <h2>I'm the problem it's me!!!</h2>
-      <Weather city="Warsaw" />
-      <a href="https://www.shecodes.io" target="_blank" rel="noreferrer">Link</a> 
-      <img src="" alt="" />
-      <p>{city}</p>
-      </header>
+<div className="App">
+  <div className="container">
+        <div className="row align-items-start">
+          <div className="col-3">
+            <h1 className="myCity">Kraków</h1>
+          </div>
+          <div className="col-7">
+            <form action="" id="form-search">
+              <input
+                className="form-control"
+                type="text"
+                placeholder="City"
+                aria-label="default input example"
+                id="write-in"
+              />{" "}
+              <button
+                type="submit"
+                className="btn btn-secondary"
+                id="searching-city"
+              >
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+  
+    <br />
+    <div className="row align-items-start">
+      <div className="col-8 bold">
+        <div>
+          <div className="today">
+            THURSDAY</div>LAST UPDATED: <span className="time"> 14:<span className="min">00</span></span><br /><span
+            className="current">CURRENT
+            WEATHER</span>
+          <br />
+        </div>
+      </div>
+      <div className="col-4">
+        <div className="addition">HUMIDITY: <span className="humid">60</span>% <br />WIND: <span className="speed">1</span>
+          m/s</div><br />
+      </div>
     </div>
+
+    <div className="row align-items-start">
+      <div className="col-2">
+        <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="weather" id="icon"></img>
+      </div>
+      <div className="col-4">
+        <h2><span className="units"><span className="actualTemp" id="celsius"></span><a href="#" id="celsiusT"
+              className="active active2">℃</a> |
+            <a href="#" id="fahrenheitT" className="active2">℉</a></span>
+        </h2>
+      </div>
+    </div>
+  </div>
+<Footer />
+</div>
+
   );
 }
 
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer (){
+    return (
+<div className="Footer">
+    <footer className="sign">Coded with ☕&🍪 by WP<br /><a href="https://github.com/becauseidontlike/Weather-App"
+    target="_blank" rel="noreferrer">Open-source
+    code</a></footer>
+    </div>
+    );
+}
